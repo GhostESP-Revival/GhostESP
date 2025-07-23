8a3f3b6a14448233022be22a5bdfcf35c1e93cde
tototo31 <Recipient7966@proton.me>
2025-07-23 13:20:15 -0500

    Add communication command handlers for discovery, connection, sending, status, disconnection, and pin configuration

---
+++ include/core/cli-handlers/comm-cli-handler.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void handle_comm_discovery(int argc, char **argv);
+void handle_comm_connect(int argc, char **argv);
+void handle_comm_send(int argc, char **argv);
+void handle_comm_status(int argc, char **argv);
+void handle_comm_disconnect(int argc, char **argv);
+void handle_comm_setpins(int argc, char **argv);
