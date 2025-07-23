59c0b01c43268ed2a83d96dc9f20780b06a89957
tototo31 <Recipient7966@proton.me>
2025-07-23 14:21:33 -0500

    Refactor command handling: Remove TP-Link command functions and PineAP detection; add misc CLI handler. Update help commands and improve code organization.

---
+++ include/core/cli-handlers/misc-cli-handler.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void handle_help(int argc, char **argv);
+void handle_startwd(int argc, char **argv);
+void handle_scan_ports(int argc, char **argv);
+void handle_web_auth_cmd(int argc, char **argv);
+void handle_pineap_detection(int argc, char **argv);
+void handle_tp_link_test(int argc, char **argv);
