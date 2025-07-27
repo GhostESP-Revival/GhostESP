ef7d5a84daa39385733845370eb3335dcd154342
tototo31 <Recipient7966+github@proton.me>
2025-07-27 14:00:57 -0500

    Refactor argument validation in GPS info command to improve clarity and error handling

--- main/core/cli-handlers/gps-cli-handler.c
+++ main/core/cli-handlers/gps-cli-handler.c
@@ -11,11 +11,20 @@ extern GPSManager g_gpsManager;
 void handle_gps_info(int argc, char **argv) {
     bool stop_flag = false;
 
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-s") == 0) {
-            stop_flag = true;
-            break;
-        }
+    // Validate arguments: only allow 0 or 1 argument, and if present, it must be "-s"
+    if (argc > 2) {
+        printf("Usage: gpsinfo [-s]\n");
+        TERMINAL_VIEW_ADD_TEXT("Usage: gpsinfo [-s]\n");
+        return;
+    }
+    if (argc == 2 && strcmp(argv[1], "-s") != 0) {
+        printf("Unknown parameter: %s\nUsage: gpsinfo [-s]\n", argv[1]);
+        TERMINAL_VIEW_ADD_TEXT("Unknown parameter: %s\nUsage: gpsinfo [-s]\n", argv[1]);
+        return;
+    }
+
+    if (argc == 2 && strcmp(argv[1], "-s") == 0) {
+        stop_flag = true;
     }
 
     if (stop_flag) {
@@ -25,6 +34,9 @@ void handle_gps_info(int argc, char **argv) {
             gps_manager_deinit(&g_gpsManager);
             printf("GPS info display stopped.\n");
             TERMINAL_VIEW_ADD_TEXT("GPS info display stopped.\n");
+        } else {
+            printf("GPS info display is not running.\n");
+            TERMINAL_VIEW_ADD_TEXT("GPS info display is not running.\n");
         }
     } else {
         if (gps_info_task_handle == NULL) {
@@ -37,6 +49,9 @@ void handle_gps_info(int argc, char **argv) {
             xTaskCreate(gps_info_display_task, "gps_info", 4096, NULL, 1, &gps_info_task_handle);
             printf("GPS info started.\n");
             TERMINAL_VIEW_ADD_TEXT("GPS info started.\n");
+        } else {
+            printf("GPS info display is already running.\n");
+            TERMINAL_VIEW_ADD_TEXT("GPS info display is already running.\n");
         }
     }
 }
