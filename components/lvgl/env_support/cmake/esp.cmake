f2b1caf54eea0f662c7dd64e8e1e90ecc24ac50f
deki <133104319+jaylikesbunda@users.noreply.github.com>
2026-09-03 01:01:34 +0800

    Update esp.cmake

--- components/lvgl/env_support/cmake/esp.cmake
+++ components/lvgl/env_support/cmake/esp.cmake
@@ -60,9 +60,11 @@ if(CONFIG_CROWPANEL_ADVANCE_S3_LCD)
   if(NOT CONFIG_IDF_TARGET_ESP32S3)
     message(FATAL_ERROR "CrowPanel SIMD requires ESP32-S3")
   endif()
-  target_sources(${COMPONENT_LIB} PRIVATE
-    "${LVGL_ROOT_DIR}/src/draw/sw/lv_draw_sw_s3_simd.S")
-  target_compile_definitions(${COMPONENT_LIB} PUBLIC GHOST_LVGL_S3_SIMD=1)
+  if(CONFIG_LV_COLOR_DEPTH_16 AND NOT CONFIG_LV_COLOR_16_SWAP)
+    target_sources(${COMPONENT_LIB} PRIVATE
+      "${LVGL_ROOT_DIR}/src/draw/sw/lv_draw_sw_s3_simd.S")
+    target_compile_definitions(${COMPONENT_LIB} PUBLIC GHOST_LVGL_S3_SIMD=1)
+  endif()
 endif()
 
 if(CONFIG_LV_ATTRIBUTE_FAST_MEM_USE_IRAM)
